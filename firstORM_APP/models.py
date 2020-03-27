9122e4429afc48b29ce1054550aad813a0c33daa
Cinque Culver <culver.ce@gmail.com>
2020-03-27 11:46:07 -0400

    Update while seeking assistance.

--- firstORM_APP/models.py
+++ firstORM_APP/models.py
@@ -8,3 +8,28 @@ class Movie(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
+
+
+
+# #SQL Queries to ORM
+# from firstORM_APP import Wizard
+
+# #Write SQL queries as ORM commands
+# Wizard.objects.create(name='Harry Potter',house='Gryffindor',pet='Hedwig',year=5)
+# Wizard.objects.create(name='Hermione Granger',house='Gryffindor',pet='Crookshanks',year=5)
+# Wizard.objects.filter(house='Gryffindor')
+
+# wiz = Wizard.objects.get(id=1)
+# wiz.year = 6
+# wiz.save
+
+# #Write ORM commands as SQL queries
+# INSERT INTO Wizard (name, house, pet, year) VALUES ('Luna Lovegood', 'Ravenclaw', 'none', 4);
+# INSERT INTO Wizard (name, house, pet, year) VALUES ('Padma Patil', 'Ravenclaw', 'none', 5);
+# SELECT house FROM Wizard WHERE house = 'Ravenclaw';
+# UPDATE Wizard SET year = 5 WHERE name = 'Luna Lovegood';
--- firstORM_APP/views.py
+++ firstORM_APP/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+# from firstORM_APP.models import Movie
 
+# def index(request):
+#     context = {
+#        'all_movies' : Movie.objects.all()
+#     }
+#     return render(request, 'index.html', context)
+
+# other imports
+# from firstORM_APP import models
+# from models import Movie
+from .models import Movie
+# show all of the data from a table
 def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+    context = {
+        "all_the_movies": Movie.objects.all()
+    }
+    return render(request, "index.html", context)
